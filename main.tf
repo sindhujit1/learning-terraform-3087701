2c95ee9f86be0505c284ab9e276b34dc60baddce
sindhujit1 <60683901+sindhujit1@users.noreply.github.com>
2023-09-10 00:45:16 -0700

    create load balancer

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
   name = "blog"
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
   ingress_rules = ["http=80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
